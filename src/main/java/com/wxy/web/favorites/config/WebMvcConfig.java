54ec0477b897e4aa57d6066b5f3f2e5a4c1f6930
wangxiaoyuan <243548880@qq.com>
2020-01-27 17:32:37 +0800

    拦截器

---
+++ src/main/java/com/wxy/web/favorites/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.wxy.web.favorites.config;
+
+import com.wxy.web.favorites.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login/**", "/register/**","/login.html","/layui/**");
+    }
+}
---
+++ src/main/java/com/wxy/web/favorites/interceptor/LoginInterceptor.java
@@ -0,0 +1,18 @@
+package com.wxy.web.favorites.interceptor;
+
+import com.wxy.web.favorites.model.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("/login.html");
+        }
+        return true;
+    }
+}
