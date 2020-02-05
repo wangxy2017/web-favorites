45eb83d11500439ff60b320f4160e8ca313cc03d
wangxiaoyuan <243548880@qq.com>
2020-02-05 16:32:59 +0800

    修改数据库

--- src/main/java/com/wxy/web/favorites/config/WebMvcConfig.java
+++ src/main/java/com/wxy/web/favorites/config/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 登录拦截
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login/**", "/register/**", "/login.html", "/layui/**", "/images/**","/h2-console/**");
+                .excludePathPatterns("/login/**", "/register/**", "/login.html", "/layui/**", "/images/**");
     }
 }
