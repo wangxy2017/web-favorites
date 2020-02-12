d8ab4b1a0d46c9ab78454704d7635b24dbcebb90
wangxiaoyuan <243548880@qq.com>
2020-02-12 12:02:09 +0800

    异步调用

--- src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
+++ src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
@@ -2,8 +2,10 @@ package com.wxy.web.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class WebFavoritesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wxy/web/favorites/util/EmailUtils.java
+++ src/main/java/com/wxy/web/favorites/util/EmailUtils.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +16,7 @@ public class EmailUtils {
     @Value("${spring.mail.username}")
     private String from;
 
+    @Async
     public void send(String to, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setSubject(title);
