c75e6262c7056f143fb645cc6a7665c528f24f3a
wangxiaoyuan <243548880@qq.com>
2020-03-10 21:30:51 +0800

    智能收藏

--- src/main/java/com/wxy/web/favorites/util/HtmlUtils.java
+++ src/main/java/com/wxy/web/favorites/util/HtmlUtils.java
@@ -1,13 +1,16 @@
 package com.wxy.web.favorites.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -18,50 +21,51 @@ public class HtmlUtils {
             .readTimeout(500, TimeUnit.MILLISECONDS)
             .build();
 
-    /**
-     * 获取网站根目录下的图标
-     *
-     * @param url
-     * @return
-     * @throws Exception
-     */
-    public static String getIcon(String url) throws Exception {
-        URL u = new URL(url);
-        String iconUrl = u.getProtocol() + "://" + u.getHost() + (u.getPort() > 0 ? ":" + u.getPort() : "") + "/favicon.ico";
-        Response response = client.newCall(new Request.Builder().url(iconUrl).build()).execute();
-        if (response.isSuccessful()) {
-            return iconUrl;
-        }
-        return null;
-    }
-
-    /**
-     * 获取html页面中的title内容
-     *
-     * @param url
-     * @return
-     * @throws Exception
-     */
-    public static String getTitle(String url) throws Exception {
-        URL u = new URL(url);
-        String rootPath = u.getProtocol() + "://" + u.getHost() + (u.getPort() > 0 ? ":" + u.getPort() : "") + "/";
-        Response response = client.newCall(new Request.Builder().url(rootPath).build()).execute();
-        if (response.isSuccessful()) {
-            String body = response.body().string();
-            Document document = Jsoup.parse(body);
-            Elements elements = document.getElementsByTag("title");
-            if (elements.size() > 0) {
-                return elements.get(0).text();
-            }
-        }
-        return null;
-    }
-
     public static Html parseUrl(String urlString) {
-        return new Html();
+        String iconUrl = "";
+        String title = "";
+        try {
+            // 获取根icon
+            URL url = new URL(urlString);
+            String rootIcon = url.getProtocol() + "://" + url.getHost() + (url.getPort() > 0 ? ":" + url.getPort() : "") + "/favicon.ico";
+            Response response = client.newCall(new Request.Builder().url(rootIcon).build()).execute();
+            if (response.isSuccessful()) {
+                iconUrl = rootIcon;
+            }
+            // 获取title
+            Response response1 = client.newCall(new Request.Builder().url(urlString).build()).execute();
+            if (response1.isSuccessful()) {
+                String body = response1.body().string();
+                Document document = Jsoup.parse(body);
+                Elements elements = document.getElementsByTag("title");
+                if (elements.size() > 0) {
+                    title = elements.get(0).text();
+                }
+                if (StringUtils.isBlank(iconUrl)) {
+                    Elements elements1 = document.getElementsByAttributeValueMatching("rel", "Shortcut Icon|shortcut icon|icon");
+                    if (elements1.size() > 0) {
+                        iconUrl = elements1.get(0).attr("href");
+                        // 判断是否相对路径
+                        if (!iconUrl.startsWith("http")) {
+                            URL url1 = new URL(urlString);
+                            if (iconUrl.startsWith("/")) {
+                                iconUrl = url1.getProtocol() + "://" + url1.getHost() + (url1.getPort() > 0 ? ":" + url1.getPort() : "") + iconUrl;
+                            } else {
+                                String path = url1.getProtocol() + "://" + url1.getHost() + (url1.getPort() > 0 ? ":" + url1.getPort() : "") + url1.getPath();
+                                iconUrl = path.substring(path.lastIndexOf("/")) + "/" + iconUrl;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Html(title, iconUrl);
     }
 
     @Data
+    @AllArgsConstructor
     public static class Html {
         String icon;
         String title;
