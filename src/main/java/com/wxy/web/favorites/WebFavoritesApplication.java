785c39bfa9ed4e50d6ed7290303075450a7df36b
wangxiaoyuan <243548880@qq.com>
2020-04-24 14:19:23 +0800

    移除缓存

--- src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
+++ src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-//@EnableCaching
 public class WebFavoritesApplication {
 
     public static void main(String[] args) {
