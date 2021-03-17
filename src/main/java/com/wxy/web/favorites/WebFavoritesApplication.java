ed4f3319d53599d713c2efcaf94adf44469435be
王孝元 <243548880@qq.com>
2021-03-17 22:36:15 +0800

    优化

--- src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
+++ src/main/java/com/wxy/web/favorites/WebFavoritesApplication.java
@@ -2,13 +2,11 @@ package com.wxy.web.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-@EnableJpaAuditing
 @EnableScheduling
 public class WebFavoritesApplication {
 
