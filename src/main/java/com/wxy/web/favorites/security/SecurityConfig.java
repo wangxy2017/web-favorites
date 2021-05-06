1ef094aa4c243a1ffa56d725334aa1cbb0141b1c
wangxiaoyuan <243548880@qq.com>
2021-05-06 16:29:00 +0800

    优化

---
+++ src/main/java/com/wxy/web/favorites/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.wxy.web.favorites.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author wangxiaoyuan
+ * 2021/5/6 16:26
+ **/
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
