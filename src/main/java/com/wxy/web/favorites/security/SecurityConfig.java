97051c128b784126e45c567a731abfe7ad396bf3
王孝元 <243548880@qq.com>
2021-05-06 21:48:54 +0800

    优化

--- src/main/java/com/wxy/web/favorites/security/SecurityConfig.java
+++ src/main/java/com/wxy/web/favorites/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.wxy.web.favorites.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author wangxiaoyuan
@@ -9,5 +12,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  **/
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 静态资源放行
+     *
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**", "/layui/**", "/favicon.ico", "/*.html");
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 放行接口
+                .antMatchers("/login/**", "/register/**").permitAll()
+                .anyRequest().authenticated()
+                // 禁用session
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 禁用csrf
+                .and().csrf().disable();
+    }
 }
