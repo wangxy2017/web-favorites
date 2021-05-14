31e66b08bca263c7349c2753096405b01e1ff869
wangxiaoyuan <243548880@qq.com>
2021-05-14 11:18:08 +0800

    优化

--- src/main/java/com/wxy/web/favorites/util/AESUtils.java
+++ src/main/java/com/wxy/web/favorites/util/AESUtils.java
@@ -1,16 +1,9 @@
 package com.wxy.web.favorites.util;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 /**
  * @author wangxiaoyuan
@@ -31,7 +24,7 @@ public class AESUtils {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//"算法/模式/补码方式"
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
         byte[] encrypted = cipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
-        return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码功能，同时能起到2次加密的作用。
+        return Base64.getEncoder().encodeToString(encrypted);//此处使用BASE64做转码功能，同时能起到2次加密的作用。
     }
 
     // 解密
@@ -48,7 +41,7 @@ public class AESUtils {
         SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-        byte[] encrypted1 = new BASE64Decoder().decodeBuffer(message);//先用base64解密
+        byte[] encrypted1 = Base64.getDecoder().decode(message);//先用base64解密
         byte[] original = cipher.doFinal(encrypted1);
         return new String(original, StandardCharsets.UTF_8);
     }
