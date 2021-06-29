00e7914f6e46bcb0a465ef39f767f94098da7cac
王孝元 <243548880@qq.com>
2021-06-30 02:48:39 +0800

    优化

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,12 +1,8 @@
 import cn.hutool.core.util.RandomUtil;
 import com.wxy.web.favorites.WebFavoritesApplication;
 import com.wxy.web.favorites.constant.PublicConstants;
-import com.wxy.web.favorites.dao.CategoryRepository;
-import com.wxy.web.favorites.dao.FavoritesRepository;
-import com.wxy.web.favorites.dao.UserRepository;
-import com.wxy.web.favorites.model.Category;
-import com.wxy.web.favorites.model.Favorites;
-import com.wxy.web.favorites.model.User;
+import com.wxy.web.favorites.dao.*;
+import com.wxy.web.favorites.model.*;
 import com.wxy.web.favorites.util.PinYinUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -16,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -37,6 +34,18 @@ public class DemoTest {
     private FavoritesRepository favoritesRepository;
 
     @Autowired
+    private MomentRepository momentRepository;
+
+    @Autowired
+    private UserFileRepository userFileRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private SearchTypeRepository searchTypeRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     /**
@@ -44,14 +53,32 @@ public class DemoTest {
      */
     @Test
     public void test() {
+        // 批量创建用户
         for (int k = 0; k < 100; k++) {
-            String key = RandomUtil.randomString(16);
             User user = userRepository.save(new User(null, "test" + k, passwordEncoder.encode("test" + k), "test" + k + "@qq.com", null, null, null, null));
+            // 批量创建分类
             for (int i = 0; i < 100; i++) {
                 Category category = categoryRepository.save(new Category(null, "test" + i, user.getId(), null, null, null, null));
+                // 批量创建收藏
                 for (int j = 0; j < 100; j++) {
                     favoritesRepository.save(new Favorites(null, "百度一下" + j, "http://www.baidu.com/favicon.ico", "http://www.baidu.com/", category.getId(), user.getId(), PinYinUtils.toPinyin("百度一下" + j), PinYinUtils.toPinyinS("百度一下" + j), null, null, null, null, null, null, null, null));
                 }
+            }
+            // 批量创建瞬间
+            for (int i = 0; i < 100; i++) {
+                momentRepository.save(new Moment(null,"测试"+i,"测试"+i,user.getId(),new Date(),null));
+            }
+            // 批量创建搜索
+            for (int i = 0; i < 100; i++) {
+                searchTypeRepository.save(new SearchType(null,"百度搜索"+i,"https://www.baidu.com/favicon.ico","https://www.bing.com/search?q=",user.getId()));
+            }
+            // 批量创建文件
+            for (int i = 0; i < 100; i++) {
+                userFileRepository.save(new UserFile(null,user.getId(),null,new Date(),new Date(),"测试"+i,null,PublicConstants.DIR_CODE,null,null));
+            }
+            // 批量创建任务
+            for (int i = 0; i < 100; i++) {
+                taskRepository.save(new Task(null,"测试"+i,new Date(),null,null,user.getId(),new Date(),PublicConstants.TASK_LEVEL_0));
             }
         }
         log.info("批量插入数据完成！！！");
