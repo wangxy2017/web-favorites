8a69ede5f03664491b7b6e14a14c3bb1b5e470a1
e-Xiaoyuan.Wang <243548880@qq.com>
2021-09-14 15:56:30 +0800

    优化

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+echo "Starting..."
 nohup java -Xms512m -Xmx512m -Xmn256m -jar web-favorites.jar > /dev/null 2>&1 &
-echo "started!"
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-pkill -f "web-favorites.jar"
-echo "stopped!"
+echo "Stopping..."
+pid=`ps -ef | grep web-favorites.jar | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+kill -9 $pid
+fi
