884fcd7eaed20bc67b32e43eb2d77ae07c1b695f
e-Xiaoyuan.Wang <243548880@qq.com>
2022-03-17 11:15:14 +0800

    优化

--- src/main/java/com/wxy/web/favorites/Application.java
+++ src/main/java/com/wxy/web/favorites/Application.java
@@ -1,18 +1,31 @@
 package com.wxy.web.favorites;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @EnableSwagger2
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Environment environment = context.getBean(Environment.class);
+        String path = Optional.ofNullable(environment.getProperty("server.servlet.context-path")).orElse("/");
+        log.info("访问地址：http://{}:{}{}", InetAddress.getLocalHost().getHostAddress(),
+                environment.getProperty("server.port"), path);
     }
 }
+
