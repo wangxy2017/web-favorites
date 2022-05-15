fa4fade7d384eb395f5c59087d272b4b009ef857
wxy <243548880@qq.com>
2022-05-15 15:49:59 +0800

    优化

---
+++ src/main/java/com/wxy/web/favorites/core/SortDto.java
@@ -0,0 +1,9 @@
+package com.wxy.web.favorites.core;
+
+import lombok.Data;
+
+@Data
+public class SortDto {
+    private Integer firstId;
+    private Integer secondId;
+}
--- src/main/java/com/wxy/web/favorites/dao/QuickNavigationRepository.java
+++ src/main/java/com/wxy/web/favorites/dao/QuickNavigationRepository.java
@@ -3,6 +3,7 @@ package com.wxy.web.favorites.dao;
 import com.wxy.web.favorites.model.QuickNavigation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +12,12 @@ import java.util.List;
 @Repository
 public interface QuickNavigationRepository extends JpaRepository<QuickNavigation, Integer>, JpaSpecificationExecutor<QuickNavigation> {
 
-    List<QuickNavigation> findAllByUserId(Integer userId);
+    List<QuickNavigation> findAllByUserIdOrderBySort(Integer userId);
 
     long countByUserId(Integer userId);
 
     void deleteAllByUserId(Integer userId);
+
+    @Query(nativeQuery = true, value = "select max(sort) from t_quick_navigation where user_id = :userId")
+    Integer getSortByUserId(Integer userId);
 }
