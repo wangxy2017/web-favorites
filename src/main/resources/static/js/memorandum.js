1caf12324805654175cb2c912d20470d63eb108b
e-Xiaoyuan.Wang <243548880@qq.com>
2022-06-21 14:15:19 +0800

    优化

--- src/main/resources/static/js/memorandum.js
+++ src/main/resources/static/js/memorandum.js
@@ -79,9 +79,9 @@ layui.use(['layer','flow','util','form'], function() {
                                     var html = '';
                                     html += '<div class="memorandum-item layui-anim layui-anim-fadein" data-id="' + item.id + '">';
                                     if(keyword){
-                                        html += '<div class="memorandum-content" data-keyword="'+ keyword +'" ondblclick="editContent(this,evnet)">'+ escape(wrapSearch(item.content,keyword)) +'</div>';
+                                        html += '<div class="memorandum-content" data-keyword="'+ keyword +'" ondblclick="editContent(this,evnet)">'+ escape(wrapSearch(item.content,keyword)).replace(/\n/g,'<br/>') +'</div>';
                                     }else{
-                                        html += '<div class="memorandum-content" ondblclick="editContent(this)">'+ escape(item.content) +'</div>';
+                                        html += '<div class="memorandum-content" ondblclick="editContent(this)">'+ escape(item.content).replace(/\n/g,'<br/>') +'</div>';
                                     }
                                     html += '</div>';
                                     lis.push(html);
