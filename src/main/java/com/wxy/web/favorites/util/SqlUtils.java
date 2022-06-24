bf910a7a9e1285675a6808d7bbda8e5f65c98f69
e-Xiaoyuan.Wang <243548880@qq.com>
2022-06-24 15:05:17 +0800

    优化

--- src/main/java/com/wxy/web/favorites/util/SqlUtils.java
+++ src/main/java/com/wxy/web/favorites/util/SqlUtils.java
@@ -1,26 +1,24 @@
 package com.wxy.web.favorites.util;
 
-/***
- * <p>
- * Description: 描述
- * </p>
- * @author wangxiaoyuan
- * 2021年11月11日
- */
+import java.util.List;
+
 public class SqlUtils {
 
+    private static final List<String> CHARS = List.of("%", "_", "\\");
+
     public static String trimAndEscape(String keyword) {
-        if(keyword == null){
+        if (keyword == null) {
             return "";
         }
-        StringBuilder sb = new StringBuilder(keyword.trim());
-        for (int i = 0; i < sb.length(); i++) {
-            String c = String.valueOf(sb.charAt(i));
-            if ("%_\\".contains(c)) {
-                sb.insert(i, "\\");
-                i++;
+        keyword = keyword.trim();
+        if (keyword.length() == 0) {
+            return keyword;
+        }
+        for (String s : CHARS) {
+            if (keyword.startsWith(s)) {
+                return "\\" + keyword;
             }
         }
-        return sb.toString();
+        return keyword;
     }
 }
